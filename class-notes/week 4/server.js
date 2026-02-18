b54ae1830864e1ab080e33ced3720147f5b4581a
Tiqon Lediard Jr. <lediardtiqon@gmail.com>
2026-02-18 18:39:18 -0500

    III

--- class-notes/week 4/server.js
+++ class-notes/week 4/server.js
@@ -7,8 +7,9 @@ const app = express();
 // All of our files that we want to be on the front-end will go inside the public
 app.use(express.static("public"));
 
-//
 app.use(express.urlencoded({ extended: true }));
+
+let guestbookMessages = [];
 
 // 1st Parameter is the url or the location where we want to get the data from. 
 // The 2nd one is the function, action to happen whne this route is hit
@@ -16,13 +17,42 @@ app.use(express.urlencoded({ extended: true }));
 
 function callback(request, response){
 
-}*/
+}
 
 app.get("/test", (request, response) => {
     response.send('my server is working!');
+});*/
+
+app.get("/", (request, response) => {
+    response.send('I set up my / route');
 });
 
-app.listen(8000, ()=>{
+app.get("/gb", (request, response) => {
+    response.sendFile('guestbook.html', { root: "./public"});
+});
+
+app.post('/sign', (request, response)=>{
+    console.log(request.body);
+
+
+    let guest = request.body.guest;
+    let message = request.body.message;
+
+    guestbookMessages.push({
+        person: guest,
+        note:message
+    })
+
+    console.log("sign-in route has been hit");
+    //response.send("hihihihi");
+    response.redirect("/gb");
+});
+
+app.get('/a;;-messages', (request, response)=>{
+    response.json({ allMessages: guestbookMessages })
+});
+
+app.listen(3000, ()=>{
     console.log('server is running');
 })
 
